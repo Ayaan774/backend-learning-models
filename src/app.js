fb86567230ac5d93e89c8c41c32daa2879b294d0
Ayaan774 <ayaansayed2012@gmail.com>
2025-04-14 13:34:42 +0530

    all production setup complete

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
+
+//middlewares
 //middleware is used using app.use()
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
@@ -19,6 +21,21 @@ app.use(express.urlencoded({extended: true, limit: "40kb"})); //encode url data
 app.use(express.static("public")); //used for assests
 
 app.use(cookieParser())
+
+
+
+//routes import
+
+import userRouter from "./routes/user.routes.js";
+
+
+//routes declaration
+
+app.use("/api/v1/users", userRouter);
+// Above code will go like:  http://localhost:8000/api/v1/users/<any field eg: register,login,etc>
+
+
+
 
 export { app };
 
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,11 +2,13 @@
 //higher order function -> function which takes function as a argument and returns a function
 
 //method -1
-const aysncHandler = (requestHandler)=>{
-  (req, res, next) =>{
+const asyncHandler = (requestHandler)=>{
+  return (req, res, next) =>{
     Promise.resolve(requestHandler(req,res,next)).catch((err)=> next(err))
   }
 }
+export { asyncHandler };
+
 //method -2
 
 /* const aysncHandler = (func) => async (req, res, next) =>{
@@ -20,5 +22,4 @@ const aysncHandler = (requestHandler)=>{
    }
 } */
 
-export {aysncHandler};
 
